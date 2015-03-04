7d1919ec67e99ba81caef6a7adce441611945ce9
mfait <mfait@thoughtworks.com>
2015-03-04 22:20:50 +0100

    show colors of closest beacon

--- BeaconDemo/ViewController.swift
+++ BeaconDemo/ViewController.swift
@@ -12,6 +12,14 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
+
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D"), identifier: "Estimotes")
+
+    let colors = [
+        13345: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        22709: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
+        45465: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +29,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.requestWhenInUseAuthorization()
         }
         
+        locationManager.startRangingBeaconsInRegion(region)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
         
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as! CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+        }
     }
 
     override func didReceiveMemoryWarning() {
